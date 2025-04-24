8ecb8c112628010580cf2dd454ab3b76d774902d
abdelaziz <abdelazizsaleh1999@gmail.com>
2025-04-25 02:09:52 +0300

    Add image management functionality (upload and delete images)

---
+++ Ecom.Application/Services/Interfaces/IImageManagementService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Ecom.Application.Services.Interfaces
+{
+    public interface IImageManagementService
+    {
+        Task<List<string>> AddImageAsync(IFormFileCollection files, string src);
+        void DeleteImageAsync(string src);
+    }
+}
