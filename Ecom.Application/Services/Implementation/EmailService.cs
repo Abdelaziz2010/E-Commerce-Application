24326d0bf99cfc2d6dd48fa53d4a5034013e6170
abdelaziz <abdelazizsaleh1999@gmail.com>
2025-05-03 17:35:14 +0300

    complete email service SendEmail method

--- Ecom.Application/Services/Implementation/EmailService.cs
+++ Ecom.Application/Services/Implementation/EmailService.cs
@@ -1,15 +1,64 @@
 ﻿using Ecom.Application.DTOs;
 using Ecom.Application.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
 
 namespace Ecom.Application.Services.Implementation
 {
     public class EmailService : IEmailService
     {
-        //SMTP
-        public Task SendEmail(EmailDTO emailDTO)
+        private readonly IConfiguration configuration;
+        public EmailService(IConfiguration configuration)
         {
-            
+            this.configuration = configuration;
+        }
 
+        //SMTP
+        public async Task SendEmail(EmailDTO emailDTO)
+        {
+
+            MimeMessage message = new MimeMessage();
+
+            message.From.Add(new MailboxAddress("My Ecom", configuration["EmailSetting:From"]));
+
+            message.Subject = emailDTO.Subject;
+
+            message.To.Add(new MailboxAddress(emailDTO.To, emailDTO.To));
+
+            message.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = emailDTO.Content
+            };
+
+            using (var smtp = new MailKit.Net.Smtp.SmtpClient())
+            {
+                try
+                {
+                    // Connect to the SMTP server
+                    await smtp.ConnectAsync(
+                        configuration["EmailSetting:Smtp"], 
+                        int.Parse(configuration["EmailSetting:Port"]),true);
+
+                    // Authenticate with the server
+                    await smtp.AuthenticateAsync(
+                        configuration["EmailSetting:UserName"],
+                        configuration["EmailSetting:Password"]);
+
+                    // Send the email
+                    await smtp.SendAsync(message);
+
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    // Disconnect and Dispose
+                    await smtp.DisconnectAsync(true);
+                    smtp.Dispose();
+                }
+            }
         }
     }
 }
