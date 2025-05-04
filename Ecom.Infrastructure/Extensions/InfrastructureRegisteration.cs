704323bc5d3c33b8dd5f128cc0809b975aa29d2e
abdelaziz <abdelazizsaleh1999@gmail.com>
2025-05-04 16:17:31 +0300

    Configure authentication services, including JWT Bearer and cookie authentication, with appropriate validation parameters and token retrieval from cookies.

--- Ecom.Infrastructure/Extensions/InfrastructureRegisteration.cs
+++ Ecom.Infrastructure/Extensions/InfrastructureRegisteration.cs
@@ -1,15 +1,15 @@
 ﻿using Ecom.Application.Interfaces.Repositories;
 using Ecom.Infrastructure.Data;
 using Ecom.Infrastructure.Implementation.Repositories;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using StackExchange.Redis;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Ecom.Infrastructure.Extensions
 {
@@ -35,8 +35,43 @@ namespace Ecom.Infrastructure.Extensions
 
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
+            services.AddAuthentication(op =>
+            {
+                op.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                op.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                op.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            }).AddCookie(op =>
+            {
+                op.Cookie.Name = "accesstoken";
+                op.Events.OnRedirectToLogin = context =>
+                {
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    return Task.CompletedTask;
+                };
+            }).AddJwtBearer(op =>
+            {
+                op.RequireHttpsMetadata = false;
+                op.SaveToken = true;
+                op.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = false,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = configuration["JWTSettings:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWTSettings:Key"])),
+                    ClockSkew = TimeSpan.Zero
+                };
+                op.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        context.Token = context.Request.Cookies["accesstoken"];
+                        return Task.CompletedTask;
+                    },
+                };
+            });
+
             return services;
         }
-
     }
 }
