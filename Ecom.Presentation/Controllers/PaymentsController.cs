bdb1e6ee996e673b88a0c529e05a58005a33b975
abdelaziz <abdelazizsaleh1999@gmail.com>
2025-05-10 22:47:26 +0300

    create WebHook endpoint to check the status of the payment

--- Ecom.Presentation/Controllers/PaymentsController.cs
+++ Ecom.Presentation/Controllers/PaymentsController.cs
@@ -1,8 +1,11 @@
 ﻿using Ecom.Application.Services.Interfaces;
 using Ecom.Domain.Entities;
+using Ecom.Domain.Entities.Orders;
+using Ecom.Presentation.Helpers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Stripe;
 
 namespace Ecom.Presentation.Controllers
 {
@@ -23,5 +26,45 @@ namespace Ecom.Presentation.Controllers
             return await _paymentService.CreateOrUpdatePaymentAsync(cartId, deliveryMethodId);
         }
 
+        const string endpointSecret = "whsec_28cc3dec50be3eaba23c0d5217e31f075148d84948bb1e7aa84452952a3a9461";
+
+        [HttpPost("WebHook")]
+        public async Task<IActionResult> UpdateOrderStatusWithStripe()
+        {
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+
+            try
+            {
+                var stripeEvent = EventUtility.ConstructEvent(json, 
+                    Request.Headers["Stripe-Signature"], endpointSecret, throwOnApiVersionMismatch: false);
+
+                PaymentIntent paymentIntent;
+
+                Order order;
+
+                // Handle the event
+                if(stripeEvent.Type == Events.PaymentIntentSucceeded)
+                {
+                    paymentIntent = stripeEvent.Data.Object as PaymentIntent;
+                    order = await _paymentService.UpdateOrderStatusToSuccess(paymentIntent.Id);
+                }
+                else if(stripeEvent.Type == Events.PaymentIntentPaymentFailed)
+                {
+                    paymentIntent = stripeEvent.Data.Object as PaymentIntent;
+                    order = await _paymentService.UpdateOrderStatusToFailed(paymentIntent.Id);
+                }
+                // ... handle other event types
+                else
+                {
+                    Console.WriteLine("Unhandled event type: {0}", stripeEvent.Type);
+                }
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseAPI(400, ex.Message));
+            }
+        }
     }
 }
