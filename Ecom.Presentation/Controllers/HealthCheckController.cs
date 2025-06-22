8d88ad2ccc539dec5018b7b052f6e0da6a12bc87
abdelaziz <abdelazizsaleh1999@gmail.com>
2025-06-22 23:36:47 +0300

    feat(health-check): add health checks for SQL Server and managed memory
    - Registered health checks for SQL Server using a simple query to verify database connectivity.
    - Implemented a custom managed memory health check to monitor application's memory usage.
    - Added JSON response formatting for health check endpoints.
    - Configured /health, /health/live, and /health/ready endpoints for general, liveness, and readiness probes.

---
+++ Ecom.Presentation/Controllers/HealthCheckController.cs
@@ -0,0 +1,47 @@
+﻿using Asp.Versioning;
+using Ecom.Presentation.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Ecom.Presentation.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class HealthCheckController : ControllerBase
+    {
+        private readonly HealthCheckService _healthCheckService;
+
+        public HealthCheckController(HealthCheckService healthCheckService)
+        {
+            _healthCheckService = healthCheckService;
+        }
+
+
+        [HttpGet()]
+        public async Task Health()
+        {
+            var report = await _healthCheckService.CheckHealthAsync();
+
+            await HealthCheckResponseWriter.WriteJsonResponse(HttpContext, report);
+        }
+
+        [HttpGet("live")]
+        public IActionResult Liveness()
+        {
+            return Ok(new
+            {
+                status = "Healthy",
+                description = "Liveness check - the app is running"
+            });
+        }
+
+        [HttpGet("ready")]
+        public async Task Readiness()
+        {
+            var report = await _healthCheckService.CheckHealthAsync(h => h.Tags.Contains("ready"));
+
+            await HealthCheckResponseWriter.WriteJsonResponse(HttpContext, report);
+        }
+    }
+}
